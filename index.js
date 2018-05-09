683974b8358a41789f1bccc2fca648e02e076407
Brad McCollum <btmcco8@gmail.com>
2018-05-09 03:31:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function happyHolidays() {
   return `Happy holidays!`
 }
-function happyHolidaysTo(name) {
+function happyHolidaysTo(Happy holidays, name) {
   return `Happy holidays, ${name}!`
 }
 function happyHolidaysTo(string) {
