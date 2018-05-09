0f7cd2d70a4e76f6bd3228041bc98f0def8a1791
Brad McCollum <btmcco8@gmail.com>
2018-05-09 03:18:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // write your code below!
 function happyHolidays(string) {
-  return `Happy holidays, ${name}!`
+  
 }
 function happyHolidaysTo(string){
-
+  return `Happy holidays, ${name!`
 }
 function holidayCountdown(string){
   
